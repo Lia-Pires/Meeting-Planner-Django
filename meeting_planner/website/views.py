4e17ed9cfb0a91f5220eba2c54ac242da786cdf0
Lia Pires <liasth.pires@gmail.com>
2022-07-13 19:50:37 -0300

    Adding pages
    Adding pages

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def welcome(request):
     return HttpResponse('Welcome to the meeting planner application')
+
+
+def date(request):
+    return HttpResponse(f"This page was served at {str(datetime.now())}")
 
 
