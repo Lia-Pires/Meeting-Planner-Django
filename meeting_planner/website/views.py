15e31212da07f560a0904032d9bb656d3c0a229d
Lia Pires <liasth.pires@gmail.com>
2022-07-21 04:20:11 -0300

    Adding home page
    Adding page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 
 
 
 def welcome(request):
     return HttpResponse('Welcome to the Meeting Planner')
+
+
+def date(request):
+    return HttpResponse(f'{str(datetime.now())}')
+
 
